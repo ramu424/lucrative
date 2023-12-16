ea31fbc8f33f905b4f707760a26e54724fcc6404
ramireddy <ramireddy424@gmail.com>
2023-12-16 14:08:36 +0530

    added curry from windows cmd

--- read.sh
+++ read.sh
@@ -6,8 +6,5 @@ echo "You looks so gorgeous"
 echo "Welcome to New Age 1"
 echo "Welcome to New Age 2"
 echo "Welcome chiry"
-<<<<<<< HEAD
 echo "Welcome Curry"
-=======
 echo "Welcome Sambar"
->>>>>>> 1144bb2ba5df12b1d9786a1d00c0f99bc446c958
